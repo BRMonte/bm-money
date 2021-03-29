4288ea9c6d0d6badd766b4a19f0fc7a8471c3ef6
Bruno Montebello <brunormontebello@hotmail.com>
2021-03-29 12:30:03 +0100

    Config MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+
+createServer({
+  routes() {
+    this.namespace = 'api'; //aqui ta escrito API pq foi oq pus como URL no fetch que fiz na Table
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
